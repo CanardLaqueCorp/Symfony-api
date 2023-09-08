994359909d896dc525ece078f7532e8012b5ee2d
maxime-antoine <maxime.antoine001@gmail.com>
2023-09-08 23:15:43 +0200

    Update get car request

--- src/Controller/CarTHController.php
+++ src/Controller/CarTHController.php
@@ -11,24 +11,35 @@ use App\Repository\CarTHRepository;
 class CarTHController extends AbstractController
 {
     /**
-     * @Route("/car/th/{id}", name="get_car_by_id")
+     * @Route("/car/th/{id}", name="get_car_th")
      */
-    public function getById(CarTHREpository $carTHRepo, $id = null): JsonResponse
+    public function getById(CarTHRepository $carTHRepo, $id = null): JsonResponse
     {
-        $car = $carTHRepo -> findOneById($id);
+        // Si l'id n'est pas spécifié on renvoie toutes les voitures thermiques
+        if ($id = null) {
+            // $cars = $carTHRepo->findAll();
 
-        if($car == null) {
             return $this->json([
                 'response' => 'notFound'
             ]);
         }
+        // Sinon on recherche la voiture correspondant à l'id
+        else {
 
-        return $this->json([
-            'response' => 'ok',
-            'id' => $car->getId(),
-            'brand' => $car->getBrand(),
-            'model' => $car->getModel(),
-        ]);
-        
+            $car = $carTHRepo->findOneById($id);
+            
+            if($car == null) {
+                return $this->json([
+                    'response' => 'notFound'
+                ]);
+            }
+            
+            return $this->json([
+                'response' => 'ok',
+                'id' => $car->getId(),
+                'brand' => $car->getBrand(),
+                'model' => $car->getModel(),
+            ]);
+        }
     }
 }
