fd71a7b211fa7e130d5621c89dc4ca293553ae13
NicoutG <123987124+NicoutG@users.noreply.github.com>
2023-09-11 15:44:19 +0200

    Update TransmissionController.php
    getById

--- src/Controller/TransmissionController.php
+++ src/Controller/TransmissionController.php
@@ -5,17 +5,51 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TransmissionRepository;
 
 class TransmissionController extends AbstractController
 {
     /**
-     * @Route("/transmission", name="app_transmission")
+     * @Route("/get/transmission/{id}", name="get_transmission_by_id", methods={"GET"})
      */
-    public function index(): JsonResponse
+    public function getById(TransmissionRepository $transmissionRepo, $id = "all"): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TransmissionController.php',
-        ]);
+        if ($id == "all") {
+            $transmissions = $transmissionRepo->findAll([], ['label' => 'ASC']);
+
+            if ($transmissions != null) {
+                $transmissionsRes = array();
+                foreach ($transmissions as $transmission) {
+                    $transmissionsRes[] = $transmission->getDataAll();
+                }
+                return $this->json([
+                    'response' => 'ok',
+                    'result' => $transmissionsRes
+                ]);
+            }
+            else {
+                return $this->json([
+                    'response' => 'notFound'
+                ]);
+            }            
+        }
+        else {
+
+            $transmission = $transmissionRepo->find($id);
+            
+            if($transmission == null) {
+                return $this->json([
+                    'response' => 'notFound'
+                ]);
+            }
+
+            $data = $transmission->getDataAll();
+            
+            return $this->json([
+                'response' => 'ok',
+                'result' => $data
+            ]);
+        }
+
     }
 }
