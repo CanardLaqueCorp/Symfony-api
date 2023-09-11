f4b4c2120714d1e6b7b4c718e2971e34a30b6aa4
NicoutG <123987124+NicoutG@users.noreply.github.com>
2023-09-11 15:31:39 +0200

    Update FuelController.php
    getById

--- src/Controller/FuelController.php
+++ src/Controller/FuelController.php
@@ -5,17 +5,51 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\FuelRepository;
 
 class FuelController extends AbstractController
 {
     /**
-     * @Route("/fuel", name="app_fuel")
+     * @Route("/get/fuel/{id}", name="get_fuel_by_id", methods={"GET"})
      */
-    public function index(): JsonResponse
+    public function getById(FuelRepository $fuelRepo, $id = "all"): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/FuelController.php',
-        ]);
+        if ($id == "all") {
+            $fuels = $fuelRepo->findAll([], ['label' => 'ASC']);
+
+            if ($fuels != null) {
+                $fuelsRes = array();
+                foreach ($fuels as $fuel) {
+                    $fuelsRes[] = $fuel->getDataAll();
+                }
+                return $this->json([
+                    'response' => 'ok',
+                    'result' => $fuelsRes
+                ]);
+            }
+            else {
+                return $this->json([
+                    'response' => 'notFound'
+                ]);
+            }            
+        }
+        else {
+
+            $fuel = $fuelRepo->find($id);
+            
+            if($fuel == null) {
+                return $this->json([
+                    'response' => 'notFound'
+                ]);
+            }
+
+            $data = $fuel->getDataAll();
+            
+            return $this->json([
+                'response' => 'ok',
+                'result' => $data
+            ]);
+        }
+
     }
 }
